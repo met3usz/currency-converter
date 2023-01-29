7c9f5ae927d99e0fa51f7da8365ab89003580bc0
Mateusz <m.wasik1303@gmail.com>
2023-01-30 00:43:19 +0100

    Added new currencies to the app
    Now you can choose the currency yourself!

---
+++ src/Form/myCurrencies.js
@@ -0,0 +1,21 @@
+export const myCurrencies = [
+  {
+    name: 'EUR',
+    avgRate: 4.7,
+  },
+
+  {
+    name: 'USD',
+    avgRate: 4.32,
+  },
+
+  {
+    name: 'GBP',
+    avgRate: 5.35,
+  },
+
+  {
+    name: 'UAH',
+    avgRate: 0.12,
+  },
+];
