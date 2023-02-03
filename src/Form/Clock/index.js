252b40f120e26d01eabacbecd2cc2436ca70ef9a
Mateusz <m.wasik1303@gmail.com>
2023-02-03 14:14:23 +0100

    Refactored useCurrentDate

--- src/Form/Clock/index.js
+++ src/Form/Clock/index.js
@@ -1,9 +1,20 @@
 import { ClockContent } from './styled';
 import { useCurrentDate } from './useCurrentDate';
 
+const clockFormat = {
+  weekday: 'long',
+  day: 'numeric',
+  month: 'long',
+  hour: '2-digit',
+  minute: '2-digit',
+  second: '2-digit',
+};
+
 const Clock = () => {
   const date = useCurrentDate();
-  return <ClockContent>Dzisiaj jest: {date}</ClockContent>;
+  const formattedDate = date.toLocaleString(undefined, clockFormat);
+
+  return <ClockContent>Dzisiaj jest: {formattedDate}</ClockContent>;
 };
 
 export default Clock;
--- src/Form/Clock/useCurrentDate.js
+++ src/Form/Clock/useCurrentDate.js
@@ -1,14 +1,5 @@
 import { useEffect, useState } from 'react';
 
-const clockFormat = {
-  weekday: 'long',
-  day: 'numeric',
-  month: 'long',
-  hour: '2-digit',
-  minute: '2-digit',
-  second: '2-digit',
-};
-
 export const useCurrentDate = () => {
   const [date, setDate] = useState(new Date());
 
@@ -22,7 +13,5 @@ export const useCurrentDate = () => {
     };
   }, [date]);
 
-  const formattedDate = date.toLocaleString(undefined, clockFormat);
-
-  return formattedDate;
+  return date;
 };
