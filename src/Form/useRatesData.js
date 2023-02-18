1d35268edb85edc4f9d23d55360bc7d9e92934ef
Mateusz <m.wasik1303@gmail.com>
2023-02-18 23:36:31 +0100

    Moved API link to a const

--- src/Form/useRatesData.js
+++ src/Form/useRatesData.js
@@ -2,14 +2,13 @@ import { useState, useEffect } from 'react';
 import axios from 'axios';
 
 export const useRatesData = () => {
+  const url = 'https://api.exchangerate.host/latest?base=PLN';
   const [ratesData, setRatesData] = useState({ state: 'loading' });
 
   useEffect(() => {
     const fetchRates = async () => {
       try {
-        const response = await axios.get(
-          'https://api.exchangerate.host/latest?base=PLN'
-        );
+        const response = await axios.get(url);
         setRatesData({
           state: 'success',
           rates: response.data.rates,
